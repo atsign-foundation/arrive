fc6d1ccfa8d2fa3c15269453e0ea5d2b836d28b9
Rishabh Verma <rishabhv1509@gmail.com>
2020-11-23 13:49:24 +0530

    initial setup

---
+++ lib/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:atsign_location_app/routes/route_names.dart';
+import 'package:atsign_location_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SetupRoutes {
+  static String initialRoute = Routes.HOME;
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      Routes.HOME: (context) => HomeScreen(),
+    };
+  }
+}
