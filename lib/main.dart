4290b34a2a5f7f64506a7c2e979294cd703dab20
Nitesh Kumar <niteshk@geekyants.com>
2021-09-17 17:02:30 +0530

    Upgraded pubspec to use git references

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
-
 import 'package:atsign_location_app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:at_utils/at_logger.dart';
 
 import 'view_models/theme_view_model.dart';
 
 void main() {
   var themeProvider = ThemeProvider();
+  AtSignLogger.root_level = 'finer';
   WidgetsFlutterBinding.ensureInitialized();
   runZoned<Future<void>>(() async {
     ThemeColor _themeColor = await themeProvider.checkTheme();
