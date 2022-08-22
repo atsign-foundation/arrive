29d7f3ade86417f8eea80a1d8ef63e910916190e
Pritam-debnath <pritamd@geekyants.com>
2022-08-22 11:54:24 +0530

    chore: changed the package for size_config

--- lib/common_components/dialog_box/location_prompt_dialog.dart
+++ lib/common_components/dialog_box/location_prompt_dialog.dart
@@ -1,5 +1,4 @@
-// import 'package:at_onboarding_flutter/services/size_config.dart';
-import 'package:atsign_location_app/services/size_config.dart';
+import 'package:at_common_flutter/services/size_config.dart';
 import 'package:at_location_flutter/location_modal/location_notification.dart';
 import 'package:atsign_location_app/services/nav_service.dart';
 import 'package:atsign_location_app/utils/constants/text_strings.dart';
